23441b2ff46db678b265144542993af23ddbe28b
Jon <durrjp1@gmail.com>
2020-06-22 14:39:52 -0500

    taxi fair predictor basic

---
+++ TaxiFairPrediction/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TaxiFairPredictionML.Model;
+
+namespace TaxiFairPrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            ModelInput input = new ModelInput()
+            {
+                Vendor_id = "CMT",
+                Rate_code = 1,
+                Passenger_count = 3,
+                Trip_distance = 10.5f,
+                Payment_type = "CRD"
+            };
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine(result.Score);
+        }
+    }
+}
